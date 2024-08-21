cc9885cad9c451279189d0d7eb10816567117bc8
Mahil Anthony <mahilsamson2016@gmail.com>
2024-08-21 18:22:54 +0530

    Update EXP-4-080.cpp

--- EXP-4-080.cpp
+++ EXP-4-080.cpp
@@ -17,8 +17,8 @@ int main()
     int f = ~a;
     cout << "The bitwise and value of 16 and 14 is: "<< c<< endl;
     cout << "The bitwise or value of 16 and 14 is: "<< d<< endl;
-    cout << "The bitwsie xor value of 16 and 14 is: "<< e<< endl;
-    cout << "The butwise not value of 16 is: "<< f<< endl;
+    cout << "The bitwise xor value of 16 and 14 is: "<< e<< endl;
+    cout << "The bitwise not value of 16 is: "<< f<< endl;
     cout << "Left shifted value of a "<< (a<<1)<< endl;
     cout << "Right shifted value "<< (b>>1)<< endl;
     return 0;
@@ -28,7 +28,7 @@ int main()
 //Output
 The bitwise and value of 16 and 14 is: 0
 The bitwise or value of 16 and 14 is: 30
-The bitwsie xor value of 16 and 14 is: 30
-The butwise not value of 16 is: -17
+The bitwise xor value of 16 and 14 is: 30
+The bitwise not value of 16 is: -17
 Left shifted value of a 32
 Right shifted value 7
