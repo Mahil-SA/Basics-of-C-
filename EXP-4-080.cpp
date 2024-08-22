8e8f3380094923591aaf04535259f1d8674181a5
Mahil Anthony <mahilsamson2016@gmail.com>
2024-08-22 23:06:15 +0530

    Update EXP-4-080.cpp

--- EXP-4-080.cpp
+++ EXP-4-080.cpp
@@ -3,7 +3,7 @@
 //PRN -  23070123080
 //Batch -  ENTC B1
 
-//Program 1
+//Program 1 (Bitwise operators)
 #include<iostream>
 using namespace std;
 
@@ -32,3 +32,28 @@ The bitwise xor value of 16 and 14 is: 30
 The bitwise not value of 16 is: -17
 Left shifted value of a 32
 Right shifted value 7
+
+//Program 2 (Bit Positions)
+include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+    int num, set, reset;
+    // Input the number and bit positions to set and reset
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Enter the bit position to set (0 to 7): ";
+    cin >> set;
+    cout << "Enter the bit position to reset (0 to 7): ";
+    cin >> reset;
+    // Display the original number in binary
+    std::cout << num << " in binary is " << std::bitset<8>(num) << std::endl;
+    // Set the bit at set_pos
+    int num_set = num | (1 << set);
+    std::cout << "Result after setting bit number " <<set<< " is " << std::bitset<8>(num_set) << std::endl;
+    // Reset the bit at reset_pos
+    int num_reset = num_set & ~(1 << reset);
+    std::cout << "Result after resetting bit number " <<reset<< " is " << std::bitset<8>(num_reset) << std::endl;
+    return 0;
+}
